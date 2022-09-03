d47211ce1ad6be52ff167fd5984104242fcce4cc
daongochieu2810 <daongochieu2810@yahoo.com.vn>
2022-09-04 04:02:50 +0800

    Add heroku db

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export DATABASE_URL='postgres://efhcrdoezlcoqj:e5fab140e8f6aa9fff97786f021003219d0f10f6abdef01f9ce6a6024595d0e5@ec2-44-207-126-176.compute-1.amazonaws.com:5432/d90qs881urf4m9'
+export DATABASE_URL='postgresql://efhcrdoezlcoqj:e5fab140e8f6aa9fff97786f021003219d0f10f6abdef01f9ce6a6024595d0e5@ec2-44-207-126-176.compute-1.amazonaws.com:5432/d90qs881urf4m9'
 if [ -d ".venv" ]
 then
     source .venv/bin/activate
