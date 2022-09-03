139a0d0e0830b7d56d1bbb61d136d3257f2586ae
daongochieu2810 <daongochieu2810@yahoo.com.vn>
2022-09-04 04:08:17 +0800

    Add heroku db

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from src import app, utils
 from src.models import Models
 
 if __name__ == '__main__':
+    from os import environ
     models = Models()
     models.createModels()
     utils.readDbFile("src/data.sql", models)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=False, port=environ.get("PORT", 5000))
